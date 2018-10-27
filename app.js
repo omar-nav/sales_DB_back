9ee56988928c869b761ef58fc828b0f22dc79591
omar-nav <omar.navarro@g.austincc.edu>
2018-10-27 00:13:39 -0500

    make it better

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ const app = express();
 const cors = require('cors')
 app.use(cors())
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());

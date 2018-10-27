7e22532a59fdb1e7d04932ac1676a07e1762783c
omar-nav <omar.navarro@g.austincc.edu>
2018-10-26 23:46:42 -0500

    make it better

---
+++ public/precache-manifest.69c6a207d24a1e48c22e6a136ffa5404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cecf6e8e84deaf90ab85",
+    "url": "/static/js/main.cecf6e8e.chunk.js"
+  },
+  {
+    "revision": "d4c589b28d25a202bfbc",
+    "url": "/static/js/1.d4c589b2.chunk.js"
+  },
+  {
+    "revision": "cecf6e8e84deaf90ab85",
+    "url": "/static/css/main.7386da33.chunk.css"
+  },
+  {
+    "revision": "d4c589b28d25a202bfbc",
+    "url": "/static/css/1.d4c56cb4.chunk.css"
+  },
+  {
+    "revision": "a742f09fec52f190e9eac3dc6ca86fef",
+    "url": "/index.html"
+  }
+];
